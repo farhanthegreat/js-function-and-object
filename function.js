6cced00a4f47f147809e76c69ee453d167d2bc5a
farhanthegreat <farhantuber@gmail.com>
2023-02-02 16:56:57 +0600

    parameter and function basic added

--- function.js
+++ function.js
@@ -1,3 +1,13 @@
-console.log(5);
-const a='amir al oud';
-console.log(a);
+//rules
+// name, bracket, second bracket(er vitore declaration)
+
+function startFan(){
+    console.log('stand up');
+    console.log('go to the switch');
+    console.log('press the switch');
+}
+//call the function;
+startFan();
+console.log('wake up early in the morning');
+console.log('ki obostha vayara');
+startFan();
---
+++ parameter.js
@@ -0,0 +1,22 @@
+function bringSingara(money){
+    console.log('koto dilen',money);
+    console.log('ei lon singara');
+}
+var taka = 400;
+
+// bringSingara(taka);
+
+function add(num1, num2){
+
+    console.log('going t0 add', num1, num2);
+}
+
+// add(125, 96);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+
+    console.log(sum);
+}
+sum(5, 6, 2, 7, 8);
