5977c5ced8952ebf130ea772d3efd82e22a67b20
farhanthegreat <farhantuber@gmail.com>
2023-02-05 00:13:55 +0600

    added too

---
+++ object.js
@@ -0,0 +1,12 @@
+var student = {
+    id: 115,
+    name: 'Solaiman', 
+    class: 9,
+    marks: 98};
+    
+var mobile = {
+    brand: 'samsung',
+    storage: '128GB',
+    camera: '24mpx',
+    price: 19000
+};
